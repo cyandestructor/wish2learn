84e68e4363028d3149d31a107b24b59f57eb17ab
Bryan Duarte <dellinspiron15BEDL@outlook.com>
2021-02-26 11:44:58 -0600

    User validator for Javascript

--- scripts/Test.js
+++ scripts/Test.js
@@ -1,55 +1,19 @@
-import Validations from './validators/Validations.js'
+import UserValidator from './validators/user/UserValidator.js'
 
-function interpretError(field, error) {
-    let message = '';
-    
-    switch (error) {
-        case Validations.Error.Alphabetic:
-            message = `${field} must be alphabetic`;
-            break;
-        case Validations.Error.Numeric:
-            message = `${field} must be numeric`;
-            break;
-        case Validations.Error.Alphanumeric:
-            message = `${field} must be alphanumeric`;
-            break;
-        case Validations.Error.MaxLength:
-            message = `${field} is too long`;
-            break;
-        case Validations.Error.MinLength:
-            message = `${field} is too short`;
-            break;
-        case Validations.Error.Match:
-            message = `${field} does not match the expression`;
-            break;
-        case Validations.Error.MinValue:
-            message = `${field} is too low`;
-            break;
-        case Validations.Error.MaxValue:
-            message = `${field} is too high`;
-            break;
-        case Validations.Error.Required:
-            message = `${field} must not be empty`;
-            break;
-        default:
-            message = 'Unknown error';
-            break;
-    }
+const user = {
+    username: 'cyandestructor',
+    name: 'Bryan',
+    lastname: 'Duarte',
+    email: 'myemail.55@mail.com',
+    password: 'Camera11!'
+};
 
-    return message;
+let userValidator = new UserValidator(user);
+let errors = userValidator.validate();
+
+if (Object.keys(errors).length) {
+    console.log(errors);
 }
-
-let value = 'ma!A1';
-let validate = new Validations(value.trim());
-
-validate.match(/.*(?=.*[A-Z])(?=.*\d)(?=.*\W).*/);
-
-let error = validate.lastError;
-if (error !== Validations.Error.None) {
-    let message = interpretError('value', error);
-    alert(message);
-}
-else
-{
-    alert('Information is correct');
+else {
+    console.log('The information is correct');
 }
---
+++ scripts/validators/user/UserValidator.js
@@ -0,0 +1,157 @@
+import Validations from '../Validations.js';
+
+export default class UserValidator
+{
+    #user;
+    #errors = {};
+
+    constructor(user)
+    {
+        this.#user = user;
+    }
+
+    static #interpretError(field, error, minLength = 0, maxLength = 0) {
+        let message = '';
+        
+        switch (error) {
+            case Validations.Error.Alphabetic:
+                message = `${field} must be alphabetic`;
+                break;
+            case Validations.Error.Numeric:
+                message = `${field} must be numeric`;
+                break;
+            case Validations.Error.Alphanumeric:
+                message = `${field} must be alphanumeric`;
+                break;
+            case Validations.Error.Required:
+                message = `${field} must not be empty`;
+                break;
+            case Validations.Error.MinLength:
+                message = `The ${field} must be at least ${minLength} characters long`;
+                break;
+            case Validations.Error.MaxLength:
+                message = `The ${field} must be less than ${maxLength} characters long`;
+                break;
+            case Validations.Error.Email:
+                message = `The ${field} must be a valid email`;
+                break;
+            default:
+                break;
+        }
+    
+        return message;
+    }
+
+    validate()
+    {
+        this.#validateUsername();
+        this.#validateEmail();
+        this.#validateName();
+        this.#validateLastname();
+        this.#validatePassword();
+
+        return this.#errors;
+    }
+
+    #validateUsername()
+    {
+        const field = 'username';
+        let username = String(this.#user[field]).trim();
+
+        const minLength = 6;
+        const maxLength = 30;
+
+        let validate = new Validations(username);
+        validate
+            .alphanumeric()
+            .minLength(minLength)
+            .maxLength(maxLength)
+            .required();
+        
+        let error = validate.lastError;
+        if (error !== Validations.Error.None) {
+            let message = UserValidator.#interpretError(field, error, minLength, maxLength);
+            this.#addError(field, message);
+        }
+    }
+
+    #validateName()
+    {
+        const field = 'name';
+        let name = String(this.#user[field]).trim();
+
+        let validate = new Validations(name);
+        validate
+            .alphabetic(true)
+            .required();
+
+        let error = validate.lastError;
+        if (error !== Validations.Error.None) {
+            let message = UserValidator.#interpretError(field, error);
+            this.#addError(field, message);
+        }
+    }
+
+    #validateLastname()
+    {
+        const field = 'lastname';
+        let lastname = String(this.#user[field]).trim();
+
+        let validate = new Validations(lastname);
+        validate
+            .alphabetic(true)
+            .required();
+
+        let error = validate.lastError;
+        if (error !== Validations.Error.None) {
+            let message = UserValidator.#interpretError(field, error);
+            this.#addError(field, message);
+        }
+    }
+
+    #validatePassword()
+    {
+        const field = 'password';
+        let password = this.#user[field];
+
+        let validate = new Validations(password);
+        validate
+            .match(/.*(?=.*[A-Z])(?=.*\d)(?=.*\W).*/)
+            .minLength(8)
+            .required();
+
+        let error = validate.lastError;
+        if (error !== Validations.Error.None) {
+            let message;
+            if (error === Validations.Error.Match) {
+                message = 'The password must have at least one uppercase, one number and one special character';
+            }
+            else {
+                message = UserValidator.#interpretError(field, error, 8);
+            }
+            this.#addError(field, message);
+        }
+    }
+
+    #validateEmail()
+    {
+        const field = 'email';
+        let email = String(this.#user[field]).trim();
+
+        let validate = new Validations(email);
+        validate
+            .email()
+            .required();
+
+        let error = validate.lastError;
+        if (error !== Validations.Error.None) {
+            let message = UserValidator.#interpretError(field, error);
+            this.#addError(field, message);
+        }
+    }
+
+    #addError(key, value)
+    {
+        this.#errors[key] = value;
+    }
+}
