48569e6200d9e1aa0260a8d774dd171c52584763
Bryan Duarte <dellinspiron15BEDL@outlook.com>
2021-03-07 23:47:30 -0600

    Tested javascript validations

--- scripts/Test.js
+++ scripts/Test.js
@@ -1,14 +1,28 @@
-import UserValidator from './validators/user/UserValidator.js'
+import UserEditorValidator from './validators/user/UserEditionValidator.js'
 import Utility from './Utility.js';
 
-document.getElementById('userRegistration').addEventListener('submit', (e) =>{
+function displayErrors(errors) {
+    const errorList = document.getElementById('error-list');
+    
+    errorList.innerHTML = '';
+
+    for (const key in errors) {
+        if (Object.hasOwnProperty.call(errors, key)) {
+            const error = errors[key];
+            errorList.innerHTML += (error + '<br>');
+        }
+    }
+}
+
+document.getElementById('form').addEventListener('submit', (e) => {
     const form = e.target;
     const user = Utility.formDataToObject(new FormData(form));
-    
-    let userValidator = new UserValidator(user);
-    let errors = userValidator.validate();
+
+    let validator = new UserEditorValidator(user);
+    let errors = validator.validate();
 
     if (!Utility.objectIsEmpty(errors)) {
+        displayErrors(errors);
         console.log(errors);
         e.preventDefault();
     }
