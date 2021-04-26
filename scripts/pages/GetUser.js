c3ceae00a149d603129ecd84e2cf34f8c563b8a4
Bryan Duarte <dellinspiron15BEDL@outlook.com>
2021-04-25 23:23:02 -0500

    Show user info on profile using the API

--- scripts/pages/GetUser.js
+++ scripts/pages/GetUser.js
@@ -2,16 +2,18 @@ import User from "../objects/User.js";
 
 const loadUser = (user) => {
   // Display user info
+  document.getElementById('userFullname').innerText = `${user.name} ${user.lastname}`;
+  document.getElementById('userDescription').innerText = user.description;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   const path = window.location.pathname;
-  const userId = path.substr(path.lastIndexOf("/")); // Ex. /users/{id}
+  const userId = path.substr(path.lastIndexOf("/") + 1); // Ex. /users/{id}
 
   const user = new User((response) => {
     if (response.ok) {
       // Load user info
-      response.json().then(loadUser(data));
+      response.json().then(loadUser);
       return;
     }
 
