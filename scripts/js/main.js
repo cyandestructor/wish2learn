c94c5e4325ad5a379c9308d54289ed9b1ea17417
erubidm <60367052+erubidm@users.noreply.github.com>
2021-05-07 16:35:36 -0500

    Cambios recientes pequeños pero ya esta mas funcional

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,55 +1,45 @@
-$(function() {
+$(function () {
+  "use strict";
 
-  'use strict';
+  $(".js-menu-toggle").click(function (e) {
+    var $this = $(this);
 
-  $('.js-menu-toggle').click(function(e) {
+    if ($("body").hasClass("show-sidebar")) {
+      $("body").removeClass("show-sidebar");
+      $this.removeClass("active");
+    } else {
+      $("body").addClass("show-sidebar");
+      $this.addClass("active");
+    }
 
-  	var $this = $(this);
-
-  	
-
-  	if ( $('body').hasClass('show-sidebar') ) {
-  		$('body').removeClass('show-sidebar');
-  		$this.removeClass('active');
-  	} else {
-  		$('body').addClass('show-sidebar');	
-  		$this.addClass('active');
-  	}
-
-  	e.preventDefault();
-
+    e.preventDefault();
   });
 
   // click outisde offcanvas
-	$(document).mouseup(function(e) {
+  $(document).mouseup(function (e) {
     var container = $(".sidebar");
     if (!container.is(e.target) && container.has(e.target).length === 0) {
-      if ( $('body').hasClass('show-sidebar') ) {
-				$('body').removeClass('show-sidebar');
-				$('body').find('.js-menu-toggle').removeClass('active');
-			}
+      if ($("body").hasClass("show-sidebar")) {
+        $("body").removeClass("show-sidebar");
+        $("body").find(".js-menu-toggle").removeClass("active");
+      }
     }
-	}); 
-
-    
-
+  });
 });
 
-function barraprogreso(){
-	/*var div1 = document.getElementById("barraprog");
+function barraprogreso() {
+  /*var div1 = document.getElementById("barraprog");
 	var vervalor = div1.getAttribute("style");*/
 
-	/*let element = document.getElementById('barraprog');
+  /*let element = document.getElementById('barraprog');
 	let elementStyle = window.getComputedStyle(element);
 	let elementWidth = elementStyle.getPropertyValue('width');
 	alert(elementWidth); */
 
+  let element = document.getElementById("barraprog");
+  let elementStyle = window.getComputedStyle(element);
+  let elementWidth = elementStyle.getPropertyValue("width");
 
-	let element = document.getElementById('barraprog');
-	let elementStyle = window.getComputedStyle(element);
-	let elementWidth = elementStyle.getPropertyValue('width');
-
-	let element3 = document.getElementById('barraprog');
-	let element4 = element3.style.width = 'calc('+elementWidth+' + 50px)';
-
+  let element3 = document.getElementById("barraprog");
+  let element4 = (element3.style.width = "calc(" + elementWidth + " + 50px)");
 }
