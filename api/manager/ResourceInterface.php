d8b6d32da786a263a3da65b92c067b3fba243ea8
Bryan Duarte <dellinspiron15BEDL@outlook.com>
2021-05-12 23:30:13 -0500

    Removed old api manager

--- api/manager/ResourceInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-    interface ResourceInterface
-    {
-        public function get(HttpRequest $request);
-        public function post(HttpRequest $request);
-        public function put(HttpRequest $request);
-        public function delete(HttpRequest $request);
-        public function defaultMethod(HttpRequest $request);
-    }
